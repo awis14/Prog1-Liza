35e35c4d656ee3c4be8ec424a69b63e5b9d3fc1c
asdfchan <asdfchan@users.noreply.github.com>
2014-11-16 21:00:17 +0100

    Update JobScheduling.java

--- src/mypack/JobScheduling.java
+++ src/mypack/JobScheduling.java
@@ -1,16 +1,35 @@
 package mypack;
 
 import java.lang.reflect.Array;
+import java.util.Random;
 
 public class JobScheduling {
-
+	
 	public static void main(String[] args) {
 		
 		int[] jobDauern = new int[] {5, 3, 3, 4, 8, 3};
+		/*
+		jobDauern = new int[100];
+		for (int i = 0; i < jobDauern.length; i++ ) {
+			Random zufallsZahlenGenerator = new Random();
+			int zufallsZahl = zufallsZahlenGenerator.nextInt(10);
+			jobDauern[i] = zufallsZahl;
+		}  
+		 */
+
 		
 		Prozessor p0 = new Prozessor();
 		Prozessor p1 = new Prozessor();
 		Prozessor p2 = new Prozessor();
+		/*
+		 Prozessor p3 = new Prozessor();
+		 Prozessor p4 = new Prozessor();
+		 Prozessor p5 = new Prozessor();
+		 Prozessor p6 = new Prozessor();
+		 Prozessor p7 = new Prozessor();
+		 Prozessor p8 = new Prozessor();
+		 Prozessor p9 = new Prozessor();
+		 */
 		
 		Prozessor[] MAXP = new Prozessor[] {p0, p1, p2};
 		
@@ -20,11 +39,11 @@ public class JobScheduling {
 		for (int i = 0; i < MAXP.length; i++) {
 			MAXP[i].druckeZeile();
 		}
-		System.out.println("laengste dauer");
+		System.out.println("Laengste Gesamtdauer: " + pListe.hoechsteDauer());
 		pListe.resetAll(MAXP);
 		
 		System.out.println("");
-
+/*
 		
 		
 		pListe.ascending(jobDauern);
@@ -58,7 +77,7 @@ public class JobScheduling {
 		
 		
 		
-		
+	*/	
 
 	}
 
