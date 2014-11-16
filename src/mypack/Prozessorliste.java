9a3541ccbbd39b9a60ce04b813ead13ac6e52af5
asdfchan <asdfchan@users.noreply.github.com>
2014-11-16 21:36:56 +0100

    Update Prozessorliste.java
    Arbeite grad zum Isolieren nur mit unsorted1 und kuerzesteDauer1

--- src/mypack/Prozessorliste.java
+++ src/mypack/Prozessorliste.java
@@ -3,17 +3,164 @@ package mypack;
 public class Prozessorliste {
 	
 	private Prozessor[] MAXP;
-	private int geringsteAuslastung;
-	
+	private int geringsteAuslastung = 0;
+	private int hoechsteDauer = 0;
+	private int kuerzesteDauer = 90;
+	private Prozessor p;
+	private int hilfe;
+	private static int processor = 0;
 	
 	Prozessorliste(Prozessor[] MAXP) {
 		this.MAXP = new Prozessor[MAXP.length];
+	}
+
+	 public void unsorted1 (int[] jobDauern) {
+		for (int i = 0; i < jobDauern.length; i++) {
+			//Prozessor p = kuerzesteDauer();
+			System.out.println(MAXP[i].berechneAktuelleDauer());
+			hilfe = kuerzesteDauer1();
+			p.hinzufuegenJob(jobDauern[i]);
+		}	
+	}
+
+	
+	public Prozessor kuerzesteDauer() {
+		for (int i = 0; i < MAXP.length; i++) {
+			 if (MAXP[i].berechneAktuelleDauer() == 0 ) {
+				 p = MAXP[i];
+				 return p;
+			 } else if (MAXP[i].berechneAktuelleDauer() < kuerzesteDauer) {
+				 p = MAXP[i];
+			 }
+		}
+		return p;
+	}
+	 
+	public int kuerzesteDauer1() {
+		for (int i = 0; i < MAXP.length; i++) {
+			 if (MAXP[i].berechneAktuelleDauer() == 0 ) {
+				 System.out.println("aktuelle dauer = 0");
+				 return i;
+			 } else if (MAXP[i].berechneAktuelleDauer() < kuerzesteDauer) {
+				 System.out.println("aktuelle dauer " + MAXP[i].berechneAktuelleDauer() + " < " + kuerzesteDauer);
+				 hilfe = i;
+			 }
+		}
+		System.out.println("hilfe gleich i: " +hilfe);
+		return hilfe;
+	}
+
+	
+	
+	
+	public void unsorted (int[] jobDauern) {
+		for (int i = 0; i < jobDauern.length; i++) {
+			
+			 if (processor + 1 < MAXP.length) {
+				System.out.println("prozessor: " + processor);
+				if (MAXP[processor + 1].laengereDauer(MAXP[processor]) == true ) {
+					MAXP[processor].hinzufuegenJob(jobDauern[i]);
+				}
+			//System.out.println("prozessor: " + MAXP[processor].getNr());
+			//System.out.println("job: " + jobDauern[i]);
+				if (processor < (MAXP.length - 1)) {
+					processor++;
+					System.out.println("prozessor: " + processor);
+				} else {
+					processor = 0;
+				}
+			} else {
+				processor = 0;
+			}
+			
+		//	System.out.println("prozessor: " + processor);
+	//		System.out.println(processor);
+		}
+	}
+	 
+	public void ascending(int[] jobDauern, Prozessor[] MAXP) {
+		java.util.Arrays.sort(jobDauern);
+        for (int i = 0; i < jobDauern.length; i++) {
+        	MAXP[processor].hinzufuegenJob(jobDauern[i]);
+        	if (processor < (MAXP.length - 1)) {
+				processor++;				
+			} else {
+				processor = 0;
+			}
+        }
+    }
+	
+	public void descending(int[] jobDauern, Prozessor[] MAXP) {
+		java.util.Arrays.sort(jobDauern);
+		int[] copyJobDauern = new int[jobDauern.length];
+		int j = 0;
+		for ( int i = (jobDauern.length - 1); i > 0; i--) {
+			copyJobDauern[j] = jobDauern[i];
+			j++;
+		} 
+		jobDauern = new int[jobDauern.length + 1];
+		jobDauern = copyJobDauern.clone();
+      for (int i = 0; i < jobDauern.length; i++) {
+        MAXP[processor].hinzufuegenJob(jobDauern[i]);
+    	  if (processor < (MAXP.length - 1)) {
+    	    processor++;				
+    	  } else {
+    		processor = 0;
+		}
+      }
+	}
+	
+	public static void random(int[] jobDauern) {
+		
+	}
+	
+	
+	public void resetAll (Prozessor[] MAXP) {
+		for (int i = 0; i < MAXP.length; i++) {
+			MAXP[i].reset();
+		}
+	}
+	
+	public int hoechsteDauer() {
+		for (int i = 0; i < MAXP.length; i++) {
+			if ( MAXP[i].berechneAktuelleDauer() > hoechsteDauer) {
+				hoechsteDauer = MAXP[i].berechneAktuelleDauer();
+			}
+		}
+		return hoechsteDauer;
+	}
+	/*
+	public void kuerzesteDauer() {
+		for (int i = 0; i < MAXP.length; i++) {
+			//if ( MAXP[i] >)
+		}
+	}
+	*/
+	
+	public int geringsteGesamtDauer(Prozessor[] MAXP) {
+		int processorShort = 0;
+		int shortestDurance = geringsteAuslastung;
+		for (int i = 0; i < MAXP.length; i++) {
+			if (MAXP[i].berechneAktuelleDauer() > 0) {
+				//System.out.println("aktuelleDauer: " + MAXP[i].berechneAktuelleDauer() + " > shortestDur: " + shortestDurance);
+				if (MAXP[i].berechneAktuelleDauer() < shortestDurance) {
+					//System.out.println("kleiner " + shortestDurance);
+					shortestDurance = MAXP[i].berechneAktuelleDauer();
+					geringsteAuslastung = shortestDurance;
+					processorShort = i;
+				}
+			} else {
+				return processorShort = i;
+			}
+			geringsteAuslastung = shortestDurance;
+		}
+		return processorShort;
 	}
 	
 	
 	public Prozessor geringsteGesamtDauer() {
 		Prozessor p = new Prozessor();
-		int[] hilf = new int[this.MAXP.length];
+		int[] hilf = new int[MAXP.length];
 		
 		for (int i = 0; i < MAXP.length; i++) {
 			hilf[i] = MAXP[i].berechneAktuelleDauer();
@@ -34,174 +181,5 @@ public class Prozessorliste {
 	}
 	
 	
-	public void unsorted (int[] jobDauern) {
-		for (int i = 0; i < jobDauern.length; i++) {
-			Prozessor p = geringsteGesamtDauer();
-			p.addJob(jobDauern[i]);
-		}	
-	}
-	
-	public void ascending(int[] jobDauern) {
-		for (int i = 0; i < jobDauern.length; i++) {
-			
-		}
-	}
-	
-	public void descending(int[] jobDauern) {
-		for (int i = 0; i < jobDauern.length; i++) {
-			
-		}
-	}
-	
-	public void randomAlgo(int[] jobDauern) {
-		for (int i = 0; i < jobDauern.length; i++) {
-			
-		}
-	}
-	 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	private static int processor = 0;
-
-	
-	public static void resetAll (Prozessor[] MAXP) {
-		for (int i = 0; i < MAXP.length; i++) {
-			MAXP[i].reset();
-		}
-	}
-	/*
-	public static int getProcessorShortest(Prozessor[] MAXP) {
-		int processorShort = 0;
-		int shortestDurance = geringsteAuslastung;
-		for (int i = 0; i < MAXP.length; i++) {
-			if (MAXP[i].berechneAktuelleDauer() > 0) {
-				//System.out.println("aktuelleDauer: " + MAXP[i].berechneAktuelleDauer() + " > shortestDur: " + shortestDurance);
-				if (MAXP[i].berechneAktuelleDauer() < shortestDurance) {
-					//System.out.println("kleiner " + shortestDurance);
-					shortestDurance = MAXP[i].berechneAktuelleDauer();
-					geringsteAuslastung = shortestDurance;
-					processorShort = i;
-				}
-			} else {
-				return processorShort = i;
-			}
-			geringsteAuslastung = shortestDurance;
-		}
-		return processorShort;
-	}
-			
-	/*		
-			if (shortestDurance <= MAXP[i].berechneAktuelleDauer()) {
-				System.out.println("shortest dur: " + shortestDurance + " <= " + MAXP[i].berechneAktuelleDauer());
-				
-				processorShort = i;
-			} else {
-				shortestDurance = MAXP[i].berechneAktuelleDauer();
-				System.out.println("shortest dur: " + shortestDurance + " <= " + MAXP[i].berechneAktuelleDauer());
-			}
-		}
-		return processorShort;
-	}
-	*/
-	/*
-	public static void unsorted (int[] jobDauern, Prozessor[] MAXP) {
-		int a = 0;
-		for (int i = 0; i < jobDauern.length; i++) {
-			a = getProcessorShortest(MAXP);
-			MAXP[a].addJob(jobDauern[i]);
-			
-			
-			/*if (processor + 1 < MAXP.length) {
-				System.out.println("prozessor: " + processor);
-				if (MAXP[processor + 1].laengereDauer(MAXP[processor]) == true ) {
-					MAXP[processor].addJob(jobDauern[i]);
-				}
-			//System.out.println("prozessor: " + MAXP[processor].getNr());
-			//System.out.println("job: " + jobDauern[i]);
-				if (processor < (MAXP.length - 1)) {
-					processor++;
-					System.out.println("prozessor: " + processor);
-				} else {
-					processor = 0;
-				}
-			} else {
-				processor = 0;
-			}
-			
-		//	System.out.println("prozessor: " + processor);
-	//		System.out.println(processor);
-		}
-	}
-	
-	public static void ascending(int[] jobDauern, Prozessor[] MAXP) {
-		java.util.Arrays.sort(jobDauern);
-        for (int i = 0; i < jobDauern.length; i++) {
-        	MAXP[processor].addJob(jobDauern[i]);
-        	if (processor < (MAXP.length - 1)) {
-				processor++;				
-			} else {
-				processor = 0;
-			}
-        }
-    }
-	
-	public static void descending(int[] jobDauern, Prozessor[] MAXP) {
-		Prozessor.druckeKopfZeile(jobDauern);
-		java.util.Arrays.sort(jobDauern);
-		int[] copyJobDauern = new int[jobDauern.length];
-		int j = 0;
-		for ( int i = (jobDauern.length - 1); i > 0; i--) {
-			copyJobDauern[j] = jobDauern[i];
-			j++;
-		} 
-		jobDauern = new int[jobDauern.length + 1];
-		jobDauern = copyJobDauern.clone();
-	//	System.out.println("jobDauer lenght: " + jobDauern.length);
-      for (int i = 0; i < jobDauern.length; i++) {
-        MAXP[processor].addJob(jobDauern[i]);
-    	  if (processor < (MAXP.length - 1)) {
-    	    processor++;				
-    	  } else {
-    		processor = 0;
-		}
-      }
-	}
-	
-	public static void random(int[] jobDauern) {
-		
-	}
-	*/
 }
+ 
